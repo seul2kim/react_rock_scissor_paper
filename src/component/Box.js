cd67724e29883395a3c92a6c07632a4374bf4459
seul2kim <kdewly715@gmail.com>
2024-08-14 23:26:47 +0900

    랜덤값 추가 및 결과표시

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React from "react";
 
-//컴포넌트는 항상 대문자로 시작
-// 위아래 spell 주의
+// //컴포넌트는 항상 대문자로 시작
+// // 위아래 spell 주의
 
 const Box = (props) => {
-    //값이 제대로 넘어오는지 확인
-    console.log(props); //오오오옹 이미지만 보여주면 된다. 
+//     //값이 제대로 넘어오는지 확인
+//     console.log(props); //오오오옹 이미지만 보여주면 된다. 
+  let result;
+  if (
+    props.title === "Computer" &&
+    props.result !== "tie" &&
+    props.result !== ""
+  ) {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+  if (props.title === "Computer") {
+    console.log("computer", result);
+  }
 
   return (
-    <div className="box">
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
-      <img src={props.item && props.item.img} className="item-img"/>  
-      <h2>WIN</h2>
+      <h2 data-testid="item-name">{props.item && props.item.name}</h2>
+      <img className="item-img" src={props.item && props.item.img} />
+      <h2>{props.result}</h2>
     </div>
   );
 };
 
-export default Box
+export default Box;
